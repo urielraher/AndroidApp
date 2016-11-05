dd73cd80ece243e38fb5590e0bf6d61fa5b7b1a4
urielraher <urielraher@hotmail.com>
2016-11-05 13:03:52 -0500

    App indice masa corporal Clase1

--- app/src/main/java/com/example/a41/imcaplication/MainActivity.java
+++ app/src/main/java/com/example/a41/imcaplication/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.a41.imcaplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView textViewResultado;
+    int color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +26,61 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.btn_calcular);
+       /* fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+         textViewResultado = (TextView) findViewById(R.id.textResultado);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+
+    public void calcularIMC(View view) {
+        Log.d(TAG,"Entra a calcular IMC con el click");
+        EditText editTextALtura = (EditText) findViewById(R.id.textEditAltura);
+        EditText editTextPeso = (EditText) findViewById(R.id.textEditPeso);
+
+        String altura = editTextALtura.getText().toString();
+        String peso = editTextPeso.getText().toString();
+        if(altura.isEmpty() && peso.isEmpty()){ return; }
+        if(altura.matches("\\d+") && peso.matches("\\d+")){
+            float cm = Integer.valueOf(altura);
+            float kg = Integer.valueOf(peso);
+            calcular(cm, kg);
+
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void calcular( float cm, float kg) {
+        cm = (cm/100);
+        float bmi = kg /(cm * cm);
+        String resultado;
+        if(bmi < 16){
+            resultado = "Tu BMI: " + bmi + "(Severamente bajo)";
+            color = Color.RED;
+        }else if(bmi < 18.5){
+            resultado = "Tu BMI: " + bmi + "( Bajo)";
+            color = Color.YELLOW;
+        }else if(bmi < 25){
+            resultado = "Tu BMI: " + bmi + "( Normal)";
+            color = Color.GREEN;
+        }else if(bmi < 30){
+            resultado = "Tu BMI: " + bmi + "( Sobre peso)";
+            color = Color.YELLOW;
+        }else{
+            resultado = "Tu BMI: " + bmi + "( Obeso)";
+            color = Color.RED;
         }
 
-        return super.onOptionsItemSelected(item);
+        textViewResultado.setText(resultado);
+        textViewResultado.setTextColor(color);
+        Log.i(TAG,"resultado: "+resultado);
+
+
     }
 }
